00e0969f95b4b4a704e051fcc655d4d6e07a65aa
嶺出湧太 <jamyukirom@gmail.com>
2021-12-05 17:24:37 +0900

    テスト修正とミドルウェア追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { LoggerMiddleware } from './middleware/logger.middleware'
   
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/nest-todo-app'), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('todos')
+  }
+}
