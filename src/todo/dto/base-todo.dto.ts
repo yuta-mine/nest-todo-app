06402aa1dd7852c08ba916b648ce28f2ebff7725
嶺出湧太 <jamyukirom@gmail.com>
2021-12-05 11:36:23 +0900

    validation追加

--- src/todo/dto/base-todo.dto.ts
+++ src/todo/dto/base-todo.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class BaseTodoDto {
+  @IsNotEmpty()
+  @IsString()
   title: string
-  description?: string
+
+  @IsNotEmpty()
+  @IsString()
+  description: string
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,5 +1,8 @@
+import { IsNotEmpty, IsDate } from 'class-validator';
 import { BaseTodoDto } from './base-todo.dto'
 
 export class UpdateTodoDto extends BaseTodoDto {
+  @IsNotEmpty()
+  @IsDate()
   completedAt: Date;
 }
