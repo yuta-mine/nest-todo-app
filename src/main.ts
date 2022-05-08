201cfda385dc7b6056f75de2da21acb1ad5e6953
嶺出湧太 <lingchuyongtai@yuta-m.local>
2022-05-08 19:37:07 +0900

    prettier

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  
-  const options = new DocumentBuilder().setTitle('Todoアプリ').build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api', app, document)
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder().setTitle('Todo app').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
