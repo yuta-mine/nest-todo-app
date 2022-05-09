0b33261d5697ced8d030548abb987b80b78c62fc
嶺出湧太 <lingchuyongtai@yuta-m.local>
2022-05-09 10:13:03 +0900

    ユーザー検索を一旦削除

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,42 +5,42 @@ import {
   Param,
   Post,
   UseFilters,
-  HttpStatus
+  HttpStatus,
 } from '@nestjs/common';
-import { UserService } from './user.service'
+import { UserService } from './user.service';
 import { BaseUserDto } from './dto/base-user.dto';
 import { CreateUserDto } from './dto/create-user.dto.';
-import { HttpExceptionFilter } from '../filter/exception.filter'
-import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { HttpExceptionFilter } from '../filter/exception.filter';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly service: UserService) { }
-  
+  constructor(private readonly service: UserService) {}
+
   @Get()
   @ApiResponse({ status: HttpStatus.OK, type: [BaseUserDto] })
   async index() {
-    return this.service.findAll()
+    return this.service.findAll();
   }
 
-  @Get('/:id')
-  @ApiResponse({ status: HttpStatus.OK, type: BaseUserDto })
-  async find(@Param() id: string) {
-    return await this.service.findOne(id)
-  }
+  // @Get('/:id')
+  // @ApiResponse({ status: HttpStatus.OK, type: BaseUserDto })
+  // async find(@Param() id: string) {
+  //   return await this.service.findOne(id)
+  // }
 
   @Post('/signup')
   @UseFilters(new HttpExceptionFilter())
-  @ApiOperation({ summary: "user signup" })
-  @ApiResponse({ status: HttpStatus.OK, type: CreateUserDto})
+  @ApiOperation({ summary: 'user signup' })
+  @ApiResponse({ status: HttpStatus.OK, type: CreateUserDto })
   async create(@Body() CreateUserDto: CreateUserDto) {
-    return await this.service.create(CreateUserDto)
+    return await this.service.create(CreateUserDto);
   }
 
   @Post('/signin')
   @UseFilters(new HttpExceptionFilter())
-  @ApiOperation({ summary: "user signin" })
+  @ApiOperation({ summary: 'user signin' })
   async signin(@Body() BaseUserDto: BaseUserDto) {
-    return await this.service.signin(BaseUserDto)
+    return await this.service.signin(BaseUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,57 +1,61 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose'
-import { Model } from 'mongoose'
-import { User, UserDocument } from './schemas/user.schemas'
-import { BaseUserDto } from './dto/base-user.dto'
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schemas';
+import { BaseUserDto } from './dto/base-user.dto';
 import { CreateUserDto } from './dto/create-user.dto.';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 
-const scrypt = promisify(_scrypt)
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private readonly model: Model<(UserDocument)>) { }
+  constructor(
+    @InjectModel(User.name) private readonly model: Model<UserDocument>,
+  ) {}
 
   async findAll(): Promise<User[]> {
     return await this.model.find().exec();
   }
 
-  async findOne(id: string): Promise<User> {
-    return await this.model.findById(id).exec();
-  }
+  // async findOne(id: string): Promise<User> {
+  //   return await this.model.findById(id).exec();
+  // }
 
   async create(CreateUserDto: CreateUserDto): Promise<User> {
-    const registerdUser = await this.model.find({ email: CreateUserDto.email }).exec()
+    const registerdUser = await this.model
+      .find({ email: CreateUserDto.email })
+      .exec();
     if (registerdUser.length) {
-      throw new BadRequestException('This email address exists.') 
+      throw new BadRequestException('This email address exists.');
     }
 
-    const salt = randomBytes(8).toString('hex')
-    const hash = await (scrypt(CreateUserDto.password, salt, 32)) as Buffer
-    const result = salt + '.' + hash.toString('hex')
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(CreateUserDto.password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
 
     return await new this.model({
       name: CreateUserDto.name,
       email: CreateUserDto.email,
       password: result,
       createdAt: new Date(),
-    }).save()
+    }).save();
   }
 
   async signin(BaseUserDto: BaseUserDto): Promise<boolean> {
-    const [registerdUser] = await this.model.find({ email: BaseUserDto.email })
+    const [registerdUser] = await this.model.find({ email: BaseUserDto.email });
     if (!registerdUser) {
-      throw new BadRequestException('No such email address')
+      throw new BadRequestException('No such email address');
     }
 
-    const [salt, storedHash] = registerdUser.password.split('.')
-    const hash = (await scrypt(registerdUser.password, salt, 32)) as Buffer
+    const [salt, storedHash] = registerdUser.password.split('.');
+    const hash = (await scrypt(registerdUser.password, salt, 32)) as Buffer;
 
     if (storedHash !== hash.toString('hex')) {
-      throw new BadRequestException('INcorrect password')
+      throw new BadRequestException('INcorrect password');
     }
 
-    return true
+    return true;
   }
 }
